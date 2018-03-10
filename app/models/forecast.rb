51f7da0a634a5ea37d7f8ec22c6fbd6019c98e4b
Raghu Betina <raghu@firstdraft.com>
2018-03-10 19:25:04 +0000

    Validate numericality of forecasts.growth

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -5,6 +5,8 @@ class Forecast < ApplicationRecord
 
   # Validations
 
+  validates :growth, :numericality => { :less_than => 1, :greater_than => -1 }
+
   validates :intl_in_53, :numericality => { :less_than => 1, :greater_than => 0 }
 
 end
