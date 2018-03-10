3193b1869256bcfa241599adb4d8315ab146a6ba
Raghu Betina <raghu@firstdraft.com>
2018-03-10 19:25:04 +0000

    Validate numericality of forecasts.dc_in_conv

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -5,6 +5,8 @@ class Forecast < ApplicationRecord
 
   # Validations
 
+  validates :dc_in_conv, :numericality => { :less_than => 1, :greater_than => 0 }
+
   validates :growth, :numericality => { :less_than => 1, :greater_than => -1 }
 
   validates :intl_in_53, :numericality => { :less_than => 1, :greater_than => 0 }
