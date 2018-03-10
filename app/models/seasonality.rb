71c660fea3786917013fc488789646f59903f2d0
Raghu Betina <raghu@firstdraft.com>
2018-03-10 19:25:04 +0000

    Validate numericality of seasonalities.conv

--- app/models/seasonality.rb
+++ app/models/seasonality.rb
@@ -5,6 +5,8 @@ class Seasonality < ApplicationRecord
 
   # Validations
 
+  validates :conv, :numericality => { :less_than => 1, :greater_than => 0 }
+
   validates :seas_ds_40, :numericality => { :less_than => 1, :greater_than => 0 }
 
   validates :seas_ds_53, :numericality => { :less_than => 1, :greater_than => 0 }
