0a27173f45836b532693c53c074694a08bff4106
Raghu Betina <raghu@firstdraft.com>
2018-03-10 19:25:29 +0000

    Whitelist columns in Seasonality for ActiveAdmin

--- app/admin/seasonalities.rb
+++ app/admin/seasonalities.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Seasonality do
+
+ permit_params :seas_ds_40, :seas_ds_53, :conv, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
